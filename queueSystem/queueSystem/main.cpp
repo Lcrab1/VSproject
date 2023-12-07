bf265f29afa588a4659ebf818025fea879a4bfcd
Asuka <1767731149@qq.com>
2023-12-07 23:35:38 +0800

    2023.12.7 数据结构实验，排队系统

---
+++ queueSystem/queueSystem/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <queue>
+#include <ctime>
+#include<iomanip>
+#include"customer.h"
+#include"time.h"
+#include"cashier.h"
+
+
+void eightToNine()
+{
+    queue<Customer>* Q = new queue<Customer>;
+    vector<queue<Customer>>* Cashier = new vector<queue<Customer>>(6);
+    Time time;
+    cout << setw(6)<<"时间" <<setw(18)<< "顾客编号" << setw(14) << "状态" << setw(16) <<"收银台编号" << setw(20) << "收银台人数" << setw(18) << "等待时间" << endl;
+    for (int i = 0; i < 60; i++)
+    {
+        enterCashier(Cashier, Q, time);
+    }
+}
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    eightToNine();
+    system("pause");
+}
