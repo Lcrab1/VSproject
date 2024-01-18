e3248eddec2012ff9687f8bfc98cf949c47f6639
Asuka <1767731149@qq.com>
2024-01-18 22:45:24 +0800

    Creat ListCtrl

---
+++ CrabRemoteServer/CrabRemoteServer/CrabRemoteServerDlg.h
@@ -0,0 +1,37 @@
+﻿
+// CrabRemoteServerDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCrabRemoteServerDlg 对话框
+class CCrabRemoteServerDlg : public CDialogEx
+{
+// 构造
+public:
+	CCrabRemoteServerDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CRABREMOTESERVER_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	CListCtrl m_ClientInfoList;
+	CListCtrl m_ServerInfoList;
+};
